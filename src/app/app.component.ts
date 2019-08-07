433a2d4df54dff5972c6a3bc4eb5faca7f30cf3e
Maks <MaksymMaksymeiu@gmail.com>
2019-08-07 18:21:48 +0300

    add Observables & Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+  constructor() {}
 
-  constructor() { }
-
+  logCountdownEnd() {
+    console.log("the countdown has finished");
+  }
 }
